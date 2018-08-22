b1ae7bebb77e139e81c174cb98b0a4787cb153e5
Alex Jivov <alexjivov@gwu.edu>
2018-08-21 21:48:02 -0400

    added friends array

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+// All possible friend data 
+var friends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://i1.rgstatic.net/ii/profile.image/389169479208960-1469796642312_Q512/Ejaz_Ahmed12.jpg",
+        "scores": [
+            6,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Brad",
+        "photo": "https://livinginatlanta.files.wordpress.com/2012/10/imag0352.jpg",
+        "scores": [
+            2,
+            3,
+            4,
+            4,
+            5,
+            3,
+            2,
+            5,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "Chad",
+        "photo": "https://i.pinimg.com/originals/6f/6c/d5/6f6cd5b9f9ed973f886c073922d2af3c.jpg",
+        "scores": [
+            5,
+            1,
+            5,
+            4,
+            5,
+            4,
+            2,
+            3,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Preston",
+        "photo": "http://cdn.totalfratmove.com/wp-content/uploads/2015/02/07b51f446978cdfe77d724b570310647.png",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Thad",
+        "photo": "https://pbs.twimg.com/profile_images/626571590738792448/sruwem1h_400x400.jpg",
+        "scores": [
+            5,
+            1,
+            2,
+            2,
+            2,
+            1,
+            5,
+            5,
+            4,
+            1
+        ]
+    },
+]
+
+// Exports the array to other files using require - you remove this, it breaks everything
+module.exports = friends; 
